f3e3edc0f3994b9e184b6c7b388447c7ea203bfa
Carlos Treviño Balboa <48106221+carlostrevinobalboa@users.noreply.github.com>
2023-04-05 22:42:21 +0200

    Update index.js
    Al usar pulsar igual, si no es un numero se resetea el input

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+let contador = 0;
 function handleSelection(value){
+    if(contador === 1 && (value === '1' || '2' || '3' || '4' || '5' || '6' || '7' || '8' || '9' || '0' ){
+        handleClear();    
+        contador = 0;
+    } else if(contador === 1){
+        contador = 0;
+    }
     document.getElementById("visualizer").value += value;
 }
 
@@ -11,4 +18,5 @@ function handleSolve(){
     let result = eval(operation);
     handleClear();
     document.getElementById("visualizer").value += result;
+    contador = 1;
 }    
