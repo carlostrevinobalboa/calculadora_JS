dccdcc2c353e0791e06c13793174183b818d1793
Carlos Treviño Balboa <48106221+carlostrevinobalboa@users.noreply.github.com>
2023-04-05 22:43:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-let contador = 0;
+
 function handleSelection(value){
-    if(contador === 1 && (value === '1' || '2' || '3' || '4' || '5' || '6' || '7' || '8' || '9' || '0' ){
-        handleClear();    
-        contador = 0;
-    } else if(contador === 1){
-        contador = 0;
-    }
     document.getElementById("visualizer").value += value;
 }
 
@@ -18,5 +12,4 @@ function handleSolve(){
     let result = eval(operation);
     handleClear();
     document.getElementById("visualizer").value += result;
-    contador = 1;
 }    
