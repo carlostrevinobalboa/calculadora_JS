8e73f6a31c906c3b21ea74e10082276ff8ae8db4
Carlos Treviño Balboa <48106221+carlostrevinobalboa@users.noreply.github.com>
2023-04-05 15:50:23 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function handleSelection(value){
+    document.getElementById("visualizer").value += value;
+}
+
+function handleClear(){
+    document.getElementById("visualizer").value = null;
+}
+
+function handleSolve(){
+    let operation = document.getElementById("visualizer").value;
+    let result = eval(operation);
+    handleClear();
+    document.getElementById("visualizer").value += result;
+}
+
+function handleSolvePressed(event){
+    if(event.keyCode == 13){
+        handleSolve();
+    } 
+}
+
+
+    
